6fde050b931d9e6a31fbebaed51a356b19dfd0ad
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2024-09-20 14:04:06 +0200

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,33 +1,32 @@
 // app.module.ts
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core'; //Decorator to define an Angular module.
+import { BrowserModule } from '@angular/platform-browser'; //Module for browser-specific services and directives.
+import { FormsModule } from '@angular/forms'; //Module for form-related directives and services.
+import { AppComponent } from './app.component'; //The main application component
+import { HomeComponent } from './home/home.component'; //Feature components.
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
-import { RouterModule } from '@angular/router';
-
-@NgModule({
+import { RouterModule } from '@angular/router'; //Module for client-side routing.
+@NgModule({ 
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
+    AboutComponent, //declarations: Array of components, directives, and pipes that belong to this module.
     PortfolioComponent,
     ContactComponent
   ],
-  imports: [
+  imports: [ //Array of modules to import into this module.
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
+    RouterModule.forRoot([ //Configures the router with routes for the application.
       { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
+      { path: 'about', component: AboutComponent }, //Route path.
       { path: 'portfolio', component: PortfolioComponent },
       { path: 'contact', component: ContactComponent }
     ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //Array of services to provide for the module (empty in this case).
+  bootstrap: [AppComponent] //: Component to bootstrap the application (the main entry point).
 })
-export class AppModule { }
+export class AppModule { } //Exports the AppModule class.
